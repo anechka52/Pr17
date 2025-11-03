9ea12a94741dfd372b3cdd4b6898bee5b84bc6de
anechka52 <anyutka.il@yandex.ru>
2025-11-03 23:58:29 +0500

    Update

---
+++ 17/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    internal class Monster : Character
+    {
+        public Monster() : base()
+        { }
+        public Monster(int maximumHealth, string name, int minimumAttackPower,
+            int maximumAttackPower) : base(maximumHealth, name,
+                minimumAttackPower, maximumAttackPower)
+        { }
+        
+    }
+}
